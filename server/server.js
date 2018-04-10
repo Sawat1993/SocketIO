66765a5649ffa222e7635d8404f725608557fe09
Sawatantra chauhan <sawantantra.chauhan.consultant@nielsen.com>
2018-04-10 15:45:41 +0530

    .

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const port = process.env.PORT || 3000;
 
 var app = express();
-app.use(express.static(path.join(__dirname , '../public'))); 
+var server = http.createServer(app);
+var io = socketIO(server);
 
-app.listen(port,() => {
+io.on('connection', (socket) => {
+    console.log('User connected');
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+})
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+server.listen(port, () => {
     console.log('Server started on port 3000');
 });
