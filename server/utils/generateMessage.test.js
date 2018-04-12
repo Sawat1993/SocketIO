b2f1f6a2fd0beff35b7c1322a3033b322d286d2d
Sawatantra chauhan <sawantantra.chauhan.consultant@nielsen.com>
2018-04-12 16:46:46 +0530

    .

--- server/utils/generateMessage.test.js
+++ server/utils/generateMessage.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-const { generateMessage } = require('./generateMessage');
+const { generateMessage, generateLocationMessage } = require('./generateMessage');
 
 describe('GenerateMessage', () => {
     it('shoul generate message', () => {
@@ -14,4 +14,20 @@ describe('GenerateMessage', () => {
         expect(message.text).toBe(text);
 
     })
-})
+});
+
+describe('GenerateMessage', () => {
+    it('shoul generate message', () => {
+        var from = 'Sawatantra';
+        var lat = 1;
+        var long = 1;
+        var url = 'https://www.google.com/maps?q=1,1';
+
+        var message = generateLocationMessage(from, lat, long);
+
+        // expect(message.createdAt).toBeA('number');
+        expect(message.from).toBe(from);
+        expect(message.url).toBe(url);
+
+    })
+});
